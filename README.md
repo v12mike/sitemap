5deacf5fe65445403dda455a3e6cc4046e870938
Mike Morrin <morrinmike@gmail.com>
2024-09-04 13:05:58 +0000

    Update to v0.2.1

--- README.md
+++ README.md
@@ -2,25 +2,22 @@
 
 This is an extension for the phpBB forums software. You need an instalation of <a href="https://github.com/phpbb/phpbb">phpBB</a> 3.1.x or 3.2.x to use this extension.
 
+[![Download](https://raw.githubusercontent.com/tas2580/privacyprotection/master/.github/button_download.png)](https://tas2580.net/downloads/phpbb-sitemap/) [![Donate](https://raw.githubusercontent.com/tas2580/privacyprotection/master/.github/button_donate.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=S8PXGAJZP9GWN)
+
 DESCRIPTION
 -------
-Creates an XML sitemap for the phpBB forum. You can access the sitemap unter <code>YOUR_FORUM/app.php/sitemap.xml</code>
-
-### SEO URLs
-If you search for URL rewriting, look at <a href="https://github.com/tas2580/seourls">this Extension</a>
+Creates an XML sitemap for a phpBB forum. You can access the sitemap at <code>YOUR_FORUM_ROOT/app.php/sitemap.xml</code> or if mod_rewite is available, at <code>YOUR_FORUM_ROOT/sitemap.xml</code>
 
 INSTALATION
 ----------
-To install this extension download it from here and upload the files in your forum under <b>/ext/tas2580/sitemap</b>.
-After that go to the Admin panel of your forum and navigate in to Customise -> Extension Management -> Extensions. Search this extension in the list of extensions and click on Enable.
+To install this extension download it from here and upload the files in your forum under <b>/ext/v12mike/sitemap</b>.
+After that go to the ACP of your forum and navigate in to Customise -> Extension Management -> Extensions. Search this extension in the list of extensions and click on Enable.
 
-After the installation you can find your sitemap under <b>{YOUR_FORUM_URL}/app.php/sitemap.xml</b> or <b>{YOUR_FORUM_URL}/sitemap.xml</b>
+After the installation you can find your sitemap under <b>{YOUR_FORUM_ROOT_URL}/app.php/sitemap.xml</b> or <b>{YOUR_FORUM_ROOT_URL}/sitemap.xml</b>
 
 SUPPORT
 -------
-You can get support for this extension on <a href="https://www.phpbb.com/community/viewtopic.php?f=456&t=2344456">phpbb.com</a>
-or in german on <a href="https://www.phpbb.de/community/viewtopic.php?f=149&t=235901">phpbb.de</a>. For more informations look at
-<a href="https://tas2580.net/downloads/phpbb-sitemap/">my Website</a>.
+You can get support for this extension on <a href="https://www.phpbb.com/community/viewtopic.php?f=456&t=2344456">phpbb.com</a>.
 
 LICENSE
 -------
--- controller/sitemap.php
+++ controller/sitemap.php
@@ -1,15 +1,20 @@
 <?php
 /**
 *
-* @package phpBB Extension - tas2580 Social Media Buttons
+* @package phpBB Extension - v12mike XML Sitemap
 * @copyright (c) 2014 tas2580 (https://tas2580.net)
+* @copyright (c) 2018 v12mike (morrinmike@gmail.com)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
-namespace tas2580\sitemap\controller;
+namespace v12mike\sitemap\controller;
 
-define('SQL_CACHE_TIME',	120);  //only update the data after X seconds to reduce sql load a little 
+define('MAX_MAP_SIZE', 200);  // topics per map file, should be no larger than 10,000
+define('INCLUDE_VIEWFORUM_PAGES', false);  // set to true to include viewforum pages in the sitemap
+define('USE_POST_TIME', true);  // set to true to fetch correct last post time for each topic page (adds processing load)
+define('POST_PAGES_PER_QUERY', 100);  // the number of post pages of posts to fetch in each query
+define('RESULTS_CACHE_TIME', 3600);  // cache time (seconds), set to 0 to disable caching
 
 use Symfony\Component\HttpFoundation\Response;
 
@@ -44,15 +49,23 @@ class sitemap
 	* @param \phpbb\db\driver\driver_interface		$db							Database object
 	* @param \phpbb\controller\helper				$helper						Helper object
 	* @param string									$php_ext					phpEx
-	* @param \phpbb_extension_manager				$phpbb_extension_manager    phpbb_extension_manager
+	* @param \phpbb_extension_manager				$phpbb_extension_manager	phpbb_extension_manager
 	*/
-	public function __construct(\phpbb\auth\auth $auth, \phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\controller\helper $helper, \phpbb\event\dispatcher_interface $phpbb_dispatcher, $php_ext, $phpbb_extension_manager)
+	public function __construct(\phpbb\auth\auth $auth,
+								\phpbb\config\config $config,
+								\phpbb\db\driver\driver_interface $db,
+								\phpbb\controller\helper $helper,
+								\phpbb\event\dispatcher_interface $phpbb_dispatcher,
+								\phpbb\content_visibility $content_visibility,
+								$php_ext,
+								$phpbb_extension_manager)
 	{
 		$this->auth = $auth;
 		$this->config = $config;
 		$this->db = $db;
 		$this->helper = $helper;
 		$this->phpbb_dispatcher = $phpbb_dispatcher;
+		$this->content_visibility = $content_visibility;
 		$this->php_ext = $php_ext;
 		$this->phpbb_extension_manager = $phpbb_extension_manager;
 
@@ -63,76 +76,134 @@ class sitemap
 	 * Generate sitemap for a forum
 	 *
 	 * @param int		$id		The forum ID
+	 * @param int		$first	The first topic ID (0 to include viewforum pages)
+	 * @param int		$last	The last topic ID
 	 * @return object
 	 */
-	public function sitemap($id)
+	public function sitemap($forum, $start)
 	{
-		if (!$this->auth->acl_get('f_list', $id))
+		if (!$this->auth->acl_get('f_list', $forum))
 		{
 			trigger_error('SORRY_AUTH_READ');
 		}
 
 		$sql = 'SELECT forum_id, forum_name, forum_last_post_time, forum_topics_approved
 			FROM ' . FORUMS_TABLE . '
-			WHERE forum_id = ' . (int) $id;
-		$result = $this->db->sql_query($sql, SQL_CACHE_TIME);
+			WHERE forum_id = ' . (int) $forum;
+		$result = $this->db->sql_query($sql);
 		$row = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
 
-		$start = 0;
-		do
+		// generate map of viewforum pages
+		if ($start == 0)
 		{
-			// URL for the forum
-			$url = $this->board_url . '/viewforum.' . $this->php_ext . '?f=' . $id;
-			if ($start > 0)
+			if (INCLUDE_VIEWFORUM_PAGES)
 			{
-				$url .= '&amp;start=' . $start;
-			}
-			$url_data[] = array(
-				'url'	=> $url,
-				'time'	=> $row['forum_last_post_time'],
-				'row'	=> $row,
-				'start'	=> $start
-			);
-			$start += $this->config['topics_per_page'];
-		}
-		while ($start < $row['forum_topics_approved']);
-
-		// Get all topics in the forum
-		$sql = 'SELECT topic_id, topic_title, topic_last_post_time, topic_posts_approved
-			FROM ' . TOPICS_TABLE . '
-			WHERE forum_id = ' . (int) $id . '
-			AND topic_visibility = ' . ITEM_APPROVED . '
-			AND topic_status <> ' . ITEM_MOVED;
-		$result = $this->db->sql_query($sql, SQL_CACHE_TIME);
-		while ($topic_row = $this->db->sql_fetchrow($result))
-		{
-			// Put forum data to each topic row
-			$topic_row['forum_id'] = $id;
-			$topic_row['forum_name'] = $row['forum_name'];
-			$topic_row['forum_last_post_time'] = $row['forum_last_post_time'];
-
-			$start = 0;
-			do
-			{
-				// URL for topic
-				$url = $this->board_url . '/viewtopic.' . $this->php_ext . '?f=' . $id . '&amp;t=' . $topic_row['topic_id'];
-				if ($start > 0)
+				$topic = 0;
+				do
 				{
-					$url .= '&amp;start=' . $start;
+					// URL for the forum
+					$url = $this->board_url . '/viewforum.' . $this->php_ext . '?f=' . $forum . ($topic ? '&amp;start=' . $topic : '');
+					$url_data[] = array(
+						'url'	=> $url,
+						'time'	=> $row['forum_last_post_time'],
+						'start'	=> $topic
+					);
+					$topic += $this->config['topics_per_page'];
 				}
-				$url_data[] = array(
-					'url'	=> $url,
-					'time'	=> $topic_row['topic_last_post_time'],
-					'row'	=> $topic_row,
-					'start'	=> $start
-				);
-				$start += $this->config['posts_per_page'];
+				while ($topic < $row['forum_topics_approved']);
 			}
-			while ($start < $topic_row['topic_posts_approved']);
+			else
+			{
+				trigger_error('UNABLE_TO_DELIVER_FILE');
+			}
 		}
-		$this->db->sql_freeresult($result);
+		else
+		{
+			// Generate map of topics within selected range
+			$sql = 'SELECT topic_id, topic_title, topic_last_post_time, topic_posts_approved
+				FROM ' . TOPICS_TABLE . '
+				WHERE forum_id = ' . (int) $forum . '
+				AND topic_id >= ' . $start . '
+				AND ' . $this->content_visibility->get_visibility_sql('topic', $forum) .'
+				AND topic_visibility = ' . ITEM_APPROVED . '
+				AND topic_status <> ' . ITEM_MOVED . '
+				ORDER BY topic_id ASC' . '
+				LIMIT ' . (int)MAX_MAP_SIZE;
+			$result = $this->db->sql_query($sql);
+			while ($topic_row = $this->db->sql_fetchrow($result))
+			{
+				// Put forum data to each topic row
+				$topic_row['forum_id'] = $forum;
 
+				if ($topic_row['topic_posts_approved'] > $this->config['posts_per_page'])
+				{
+					// For topics with multiple pages of posts, generate an entry for each page
+					$post_offset = $post_fetch_offset = 0;
+					$last_post_offset = $this->config['posts_per_page'] - 1;
+					$posts_to_fetch = $this->config['posts_per_page'] * POST_PAGES_PER_QUERY;
+					// find the post time of the last post on each topic page
+					do
+					{
+						if (USE_POST_TIME)
+						{
+							// fetch upto $posts_to_fetch posts from the db
+							$post_fetch_offset += $posts_to_fetch;
+							$sql = 'SELECT p.post_time
+								FROM ' . POSTS_TABLE . ' p' . '
+								WHERE p.topic_id = ' . $topic_row['topic_id'] . '
+								AND ' . $this->content_visibility->get_visibility_sql('post', $forum, 'p.');
+							$posts_data = $this->db->sql_query_limit($sql, $posts_to_fetch, $post_fetch_offset, RESULTS_CACHE_TIME);
+						}
+						do
+						{
+							// URL for topic
+							$url = $this->board_url . '/viewtopic.' . $this->php_ext . '?t=' . $topic_row['topic_id'] . ($post_offset ?  '&amp;start=' . $post_offset : '');
+							$post_offset += $this->config['posts_per_page'];
+							if (USE_POST_TIME && ($post_offset < $topic_row['topic_posts_approved']))
+							{
+								$db_time = $this->db->sql_fetchfield('post_time', $post_offset - 1, $posts_data);
+								if ($db_time === false)
+								{
+									// if the (possibly cached) data set does not have the record we need, use the value from the topic table
+									$time = $topic_row['topic_last_post_time'];
+								}
+								else
+								{
+									$time = $db_time;
+								}
+							}
+							else
+							{
+								// for the last page, use the time from the topic table
+								$time = $topic_row['topic_last_post_time'];
+							}
+							$url_data[] = array(
+								'url'	=> $url,
+								'time'	=> $time,
+								'start'	=> $post_offset
+							);
+						} while (($post_offset < $topic_row['topic_posts_approved']) && // loop over pages of a topic
+								 ($post_offset < $post_fetch_offset + $posts_to_fetch)); // but not beyond posts fetched
+
+						if (USE_POST_TIME)
+						{
+							$this->db->sql_freeresult($posts_data);
+						}
+					} while (USE_POST_TIME && ($post_fetch_offset < $topic_row['topic_posts_approved']));  // loop over groups of pages of really long topics
+				}
+				else
+				{
+					// topic with a single page of posts
+					$url_data[] = array(
+						'url'	=> $this->board_url . '/viewtopic.' . $this->php_ext . '?t=' . $topic_row['topic_id'],
+						'time'	=> $topic_row['topic_last_post_time'],
+						'start'	=> 0
+					);
+				}
+			}
+			$this->db->sql_freeresult($result);
+		}
 		return $this->output_sitemap($url_data, 'urlset');
 	}
 
@@ -146,18 +217,58 @@ class sitemap
 		$sql = 'SELECT forum_id, forum_name, forum_last_post_time
 			FROM ' . FORUMS_TABLE . '
 			WHERE forum_type = ' . (int) FORUM_POST . '
-			ORDER BY left_id ASC';
-		$result = $this->db->sql_query($sql, SQL_CACHE_TIME);
+			ORDER BY forum_id ASC';
+		$result = $this->db->sql_query($sql);
 		while ($row = $this->db->sql_fetchrow($result))
 		{
-			if ($this->auth->acl_get('f_list', $row['forum_id']))
+			$forum_id = $row['forum_id'];
+			if ($this->auth->acl_get('f_list', $forum_id))
 			{
-				$url_data[] = array(
-					'url'		=> $this->helper->route('tas2580_sitemap_sitemap', array('id' => $row['forum_id']), true, '', \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL),
-					'time'		=> $row['forum_last_post_time'],
-					'row'		=> $row,
-					'start'		=> 0
-				);
+				// optionally we can add viewforum pages to the map
+				if (INCLUDE_VIEWFORUM_PAGES)
+				{
+					$url_data[] = array(
+						'url'		=> $this->helper->route('v12mike_sitemap_sitemap',
+															array('forum' => $forum_id, 'start' => 0),
+															true,
+															'',
+															\Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL),
+						'time'		=> $row['forum_last_post_time'],
+						'row'		=> $row,
+						'start'		=> 0
+					);
+				}
+				// break the topics into smaller maps if required
+				$begin_topic_id = 1;
+				do
+				{
+					// Get all topics in the forum
+					$sql = 'SELECT COUNT(topic_set.topic_id) as count, MAX(topic_set.topic_last_post_time) as latest_time, MAX(topic_set.topic_id) as end_topic_id
+						FROM (
+						SELECT topic_id, topic_last_post_time
+						FROM ' . TOPICS_TABLE . '
+						WHERE forum_id = ' . (int) $forum_id . '
+						AND topic_id >= ' . (int) $begin_topic_id . '
+						ORDER BY topic_id ASC
+						LIMIT ' . (int) MAX_MAP_SIZE . ')
+						AS topic_set' ;
+					$topic_data = $this->db->sql_query($sql);
+					$topic_row = $this->db->sql_fetchrow($topic_data);
+					$this->db->sql_freeresult($topic_data);
+					if ($topic_row['count'])
+					{
+						$end_topic_id = $topic_row['end_topic_id'];
+						$url_data[] = array(
+							'url'		=> $this->helper->route('v12mike_sitemap_sitemap',
+																array('forum' => $forum_id, 'start' => $begin_topic_id),
+																true,
+																'',
+																\Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL),
+							'time'		=> $topic_row['latest_time'],
+						);
+						$begin_topic_id = $end_topic_id + 1;
+					}
+				} while ($topic_row['count']);
 			}
 		}
 		$this->db->sql_freeresult($result);
@@ -173,25 +284,11 @@ class sitemap
 	 */
 	private function output_sitemap($url_data, $type = 'sitemapindex')
 	{
-		$style_xsl = $this->board_url . '/'. $this->phpbb_extension_manager->get_extension_path('tas2580/sitemap', false) . 'style.xsl';
+		$style_xsl = $this->board_url . '/'. $this->phpbb_extension_manager->get_extension_path('v12mike/sitemap', false) . 'style.xsl';
 
 		$xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 		$xml .= '<?xml-stylesheet type="text/xsl" href="' . $style_xsl . '" ?>' . "\n";
 		$xml .= '<' . $type . ' xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
-
-		/**
-		* Modify the sitemap link before output
-		*
-		* @event tas2580.sitemap.modify_before_output
-		* @var	string	type			Type of the sitemap (sitemapindex or urlset)
-		* @var	array		url_data		URL informations
-		* @since 0.1.4
-		*/
-		$vars = array(
-			'type',
-			'url_data',
-		);
-		extract($this->phpbb_dispatcher->trigger_event('tas2580.sitemap.modify_before_output', compact($vars)));
 
 		$tag = ($type == 'sitemapindex') ? 'sitemap' : 'url';
 		foreach ($url_data as $data)
